7c1dd7bdcec4b336b4f813331813d54f51ab164f
biah <bcunha1202@gmail.com>
2023-10-21 22:03:07 -0300

    content created

---
+++ src/app/pages/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    "id":"0",
+    "title": "Capitão América: O Primeiro Vingador",
+    "description": "Na trama, quando uma aterrorizante força ameaça a todos, em todo o planeta, o maior soldado do mundo declara guerra contra a diabólica organização Hydra, liderada pelo vilão Caveira Vermelha. Repleto de ação fantástica, deslumbrantes efeitos visuais e um inspirador herói, imbuído de coragem, humanidade e indestrutível força de vontade, o filme transporta o público a um mundo de ousadas conquistas e encantamento.",
+    "photoCover":"http://www.cadeoleo.com.br/wp-content/uploads/2012/01/AF_CA_wallpaper_1920x1080_5-1024x576.jpg"
+  },
+  {
+    "id":"1",
+    "title": "Ms. Marvel: ",
+    "description": "Nas histórias em quadrinhos, Kamala é uma jovem heroína de origem paquistanesa que idolatra a Capitã Marvel (vivida nas telas por Brie Larson). Depois de um incidente, a personagem se transforma literalmente em Carol Danvers, desenvolve habilidades extraordinárias e decide adotar o alter-ego Ms. Marvel. ",
+    "photoCover":"https://tangerina.uol.com.br/_next/image/?url=https%3A%2F%2Ffeira.tangerina.news%2Fwp-content%2Fuploads%2F2022%2F03%2Fms-marvel-nova-serie-1-1296x810.jpg&w=1200&q=75"
+  },
+
+  {
+    "id":"2",
+    "title": "HOMEM-ARANHA: ATRAVÉS DO ARANHAVERSO ",
+    "description": "Homem-Aranha: Através do Aranhaverso, é a continuação do vencedor do Oscar Homem-Aranha: No Aranhaverso, de 2018, que acompanha Miles Morales (Shameik Moore), o simpático Homem-Aranha do Brooklyn. Neste novo capítulo, Miles está de volta para uma nova missão em sua agitada vida como super herói. No novo filme, Morales é transportado para uma aventura épica através do multiverso, e deve unir forças com a mulher-aranha Gwen Stacy (Hailee Steinfeld) e um novo time de Pessoas-Aranha, formado por heróis de diversas dimensões.",
+    "photoCover":"https://www.mundoconectado.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?format=webp&quality=91&imagick=/wp-content/uploads/2023/06/tom-holland-homem-aranha.jpg"
+  },
+
+  {
+    "id":"3",
+    "title": "Legends: Marvel e Disney Plus anunciam nova série com momentos icônicos de super-heróis",
+    "description": "Agora ela está anunciando mais uma série chamada Legends, que contará momentos icônicos que são essenciais para que os telespectadores não percam nenhum detalhe de todas as produções.",
+    "photoCover":"https://observatoriodatv.uol.com.br/wp-content/uploads/2018/05/legends-of-tomorrow.jpg"
+  },
+
+
+]
